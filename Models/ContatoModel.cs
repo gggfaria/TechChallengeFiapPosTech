8a2ac30809ffd0f00f20a16bf057f50d61b965b4
Jefferson Ferreira <jefferson14489@gmail.com>
2024-04-11 16:49:50 -0300

    wip

---
+++ Models/ContatoModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechChallengeContatos.Models
+{
+    public class ContatoModel
+    {
+
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O DDD é obrigatório")]
+        public string DDD { get; set; }
+
+        [Required(ErrorMessage = "O Telefone é obrigatório")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "O E-mail é obrigatório")]
+        [EmailAddress(ErrorMessage = "E-mail em formato inválido.")]
+        public string Email { get; set; }
+    }
+}
